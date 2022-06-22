0bb64570939fb489e0f51e86457d2df8683b2509
Wilson dos Santos <wilsonsantosnet@gmail.com>
2022-06-22 14:58:15 -0300

    add spring boot sample

---
+++ springboot/singlewebappbus/src/main/java/com/example/demoservicebus/Controller/QueueReceiveController.java
@@ -0,0 +1,19 @@
+package com.example.demoservicebus.Controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QueueReceiveController {
+
+    private static final String QUEUE_NAME = "queuesampleseed";
+
+    private final Logger logger = LoggerFactory.getLogger(QueueReceiveController.class);
+
+    @JmsListener(destination = QUEUE_NAME, containerFactory = "jmsListenerContainerFactory")
+    public void receiveMessage(User user) {
+        logger.info("Received message: {}", user.getName());
+    }
+}
---
+++ springboot/singlewebappbus/src/main/java/com/example/demoservicebus/DemoServiceBusApplication.java
@@ -0,0 +1,11 @@
+package com.example.demoservicebus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoServiceBusApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoServiceBusApplication.class, args);
+    }
+}
